8979dd7d10f7f3e1dac35fa4bcf32babf0e11122
Jeffrey McFarland <jlmcfarlandj@gmail.com>
2020-02-29 11:56:59 -0600

    created layout for quiz and grabbed each button by class names

---
+++ script.js
@@ -0,0 +1,13 @@
+
+// grab each button by class and set equal to a variable.
+var btnStart = document.querySelector(".btnStart");
+var question = document.querySelector(".question");
+var btn1 = document.querySelector(".option1");
+var btn2 = document.querySelector(".option2");
+var btn3 = document.querySelector(".option3");
+var btn4 = document.querySelector(".option4");
+
+btnStart.addEventListener("click", function() {
+    alert("hello");
+
+})
